9ad1867de8de6439f3f02645823bd4f97b2d2233
Marc <x21130744@student.ncirl.ie>
2021-11-24 17:16:05 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+for (i=99;i>0;i--){
+	console.log(i+ "bottles of beer on the wall, " + i + "bottles of beer.");
+				console.log("You take one down and pass it around, " +(i-1)+ "bottles of beer on the wall");
+			}
+
+			
